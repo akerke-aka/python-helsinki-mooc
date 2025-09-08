47f4f1e97b235a485c49ea5c0a304a0fc2f0ae54
akerke-aka <akerke.koishieva02@gmail.com>
2025-09-08 21:18:07 +0500

    Update city_bikes.py

--- part6/city_bikes.py
+++ part6/city_bikes.py
@@ -1,11 +1,23 @@
+"""
+City bikes
+----------
+Functions for working on a file containing location data from the stations for city bikes in Helsinki.
+
+Functions:
+- get_station_data(filename: str): reads the names and locations of all the stations in the file, and returns them in a dictionary
+- distance(stations: dict, station1: str, station2: str): returns the distance between the two stations given as arguments
+- greatest_distance(stations: dict):  returns a tuple, with the two stations with the maximum distance
+"""
 def get_station_data(filename: str):
     stations = {}
     with open(filename) as new_file:
         for line in new_file:
             parts = line.strip().split(';')
+            # skips header line
             if parts[0] == "Longitude" and parts[1]== "Latitude":
                 continue
             else:
+                # use station name as key, coordiantes as value
                 stations[parts[3]] = (float(parts[0]), float(parts[1]))
     return stations 
 
