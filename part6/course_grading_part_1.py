a78dd54c6dcbdcf322031d2f4dd856fc634f460c
akerke-aka <akerke.koishieva02@gmail.com>
2025-09-08 21:36:21 +0500

    Update course_grading_part_1.py

--- part6/course_grading_part_1.py
+++ part6/course_grading_part_1.py
@@ -1,7 +1,18 @@
+""" Course grading, part 1
+--------------------------
+Reads student information data and exercise completion data from two CSV files.
+Returns the total number of exercises completed by each student.
+
+Steps:
+- reads student names and IDs from file
+- reads exercise completion data from file
+- aggregates and prints results
+"""
 
 student_info = input("Student information: ")
 exercise_data = input("Exercises completed: ")
 
+# dictionary for student IDs -> name
 names = {}
 with open(student_info) as new_file:    
     for line in new_file:
@@ -10,7 +21,7 @@ with open(student_info) as new_file:
             continue
         else:
             names[parts[0]] = parts[1] + " " + parts[2]
-
+# dictionary for student IDs -> total exercises
 exercises = {}
 with open(exercise_data) as new_file:
     for line in new_file:
@@ -20,6 +31,7 @@ with open(exercise_data) as new_file:
         else:
             grades_sum = sum(int(value) for value in parts[1:])
             exercises[parts[0]]=grades_sum
+# print summary
 for id, name in names.items():
     if id in exercises:
         grades_sum = exercises[id]
