f9a6d3259c2d17135ccd76939ea203eb2e19c765
akerke-aka <akerke.koishieva02@gmail.com>
2025-09-10 21:59:40 +0500

    Create Inscription.py

---
+++ part6/Inscription.py
@@ -0,0 +1,9 @@
+"""
+Inscription
+-----------
+program which asks for the name of the user and then creates an "inscription" in a file specified by the user.
+"""
+name = input("Whom should I sign this to: ")
+filename = input("Where shall I save it: ")
+with open(filename, "w") as my_file:
+    my_file.write(f"Hi {name}, we hope you enjoy learning Python with us! Best, Mooc.fi Team")
